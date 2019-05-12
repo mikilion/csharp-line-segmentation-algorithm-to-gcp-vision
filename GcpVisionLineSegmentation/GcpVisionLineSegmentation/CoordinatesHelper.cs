5207af077e6722aa98144daa643ffce0def928d3
Michele Liotino <mike.liotino@gmail.com>
2019-05-12 18:47:53 +0200

    first code conversion

---
+++ GcpVisionLineSegmentation/GcpVisionLineSegmentation/CoordinatesHelper.cs
@@ -0,0 +1,185 @@
+﻿using Google.Cloud.Vision.V1;
+using System;
+using System.Collections.Generic;
+
+namespace GcpVisionLineSegmentation
+{
+  internal class CoordinatesHelper
+  {
+    /// <summary>
+    /// Inverts the Y axis coordinates for easier computation
+    /// as the google vision starts the Y axis from the bottom
+    /// </summary>
+    /// <param name="data"></param>
+    /// <param name="yMax"></param>
+    public static void InvertAxis(IReadOnlyList<EntityAnnotation> data, int yMax)
+    {
+      // data = fillMissingValues(data);
+      foreach (EntityAnnotation entityAnnotation in data)
+      {
+        foreach (Vertex vertex in entityAnnotation.BoundingPoly.Vertices)
+        {
+          vertex.Y = yMax - vertex.Y;
+        }
+      }
+    }
+
+    public static void GetBoundingPolygon(List<ReceiptEntityAnnotation> mergedArray)
+    {
+      foreach (var item in mergedArray)
+      {
+        // calculate line height
+        int h1 = item.Vertices[0].Y - item.Vertices[3].Y;
+        int h2 = item.Vertices[1].Y - item.Vertices[2].Y;
+        int h = h2 > h1 ? h2 : h1;
+        double avgHeight = h * 0.6;
+
+        VertexDouble[] arr1 = new VertexDouble[]
+        {
+          new VertexDouble(item.Vertices[1]),
+          new VertexDouble(item.Vertices[0])
+        };
+        Line line1 = GetLine(arr1, false, avgHeight, true);
+
+        VertexDouble[] arr2 = new VertexDouble[]
+        {
+          new VertexDouble(item.Vertices[2]),
+          new VertexDouble(item.Vertices[3])
+        };
+        Line line2 = GetLine(arr2, false, avgHeight, false);
+
+        item.BigBB = CreateRectCoordinates(line1, line2);
+      }
+    }
+
+    static VertexDouble[] CreateRectCoordinates(Line line1, Line line2)
+    {
+      return new VertexDouble[]
+      {
+        new VertexDouble(line1.Xmin, line1.Ymin),
+        new VertexDouble(line1.Xmax, line1.Ymax),
+        new VertexDouble(line2.Ymax, line2.Ymax),
+        new VertexDouble(line2.Xmin, line2.Ymin)
+      };
+    }
+
+    static Line GetLine(VertexDouble[] v, bool isRoundValues, double avgHeight, bool isAdd)
+    {
+      if (isAdd)
+      {
+        v[1].Y = v[1].Y + avgHeight;
+        v[0].Y = v[0].Y + avgHeight;
+      }
+      else
+      {
+        v[1].Y = v[1].Y - avgHeight;
+        v[0].Y = v[0].Y - avgHeight;
+      }
+
+      double yDiff = v[1].Y - v[0].Y;
+      double xDiff = v[1].X - v[0].X;
+
+      double gradient = yDiff / xDiff;
+
+      int xThreshMin = 1;
+      int xThreshMax = 2000;
+
+      double yMin;
+      double yMax;
+      if (gradient == 0)
+      {
+        // extend the line
+        yMin = v[0].Y;
+        yMax = v[0].Y;
+      }
+      else
+      {
+        yMin = v[0].Y - (gradient * (v[0].X - xThreshMin));
+        yMax = v[0].Y + (gradient * (xThreshMax - v[0].X));
+      }
+      if (isRoundValues)
+      {
+        yMin = Math.Round(yMin);
+        yMax = Math.Round(yMax);
+      }
+      return new Line { Xmin = xThreshMin, Xmax = xThreshMax, Ymin = yMin, Ymax = yMax };
+    }
+
+    internal static void CombineBoundingPolygon(List<ReceiptEntityAnnotation> mergedArray)
+    {
+      // select one word from the array
+      for (int i = 0; i < mergedArray.Count; i++)
+      {
+        var bigBB = mergedArray[i].BigBB;
+
+        // iterate through all the array to find the match
+        for (int k = i; k < mergedArray.Count; k++)
+        {
+          // Do not compare with the own bounding box and which was not matched with a line
+          if (k != i && mergedArray[k].Matched == false)
+          {
+            int insideCount = 0;
+            foreach (var coordinate in mergedArray[k].Vertices)
+            {
+              if (IsPointInRectangle(coordinate, bigBB))
+              {
+                ++insideCount;
+              }
+            }
+            // all four point were inside the big bb
+            if (insideCount == 4)
+            {
+              var match = new MatchLine { MatchCount = insideCount, MatchLineNum = k };
+              mergedArray[i].Match.Add(match);
+              mergedArray[k].Matched = true;
+            }
+          }
+        }
+      }
+    }
+
+    static bool IsPointInRectangle(Vertex coordinate, VertexDouble[] rectangle)
+    {
+      bool inside = false;
+      for (int i = 0, j = 3; i < 4; j = i++)
+      {
+        if (((rectangle[i].Y <= coordinate.Y && coordinate.Y < rectangle[j].Y) || (rectangle[j].Y <= coordinate.Y && coordinate.Y < rectangle[i].Y)) &&
+           (coordinate.X < (rectangle[j].X - rectangle[i].X) * (coordinate.Y - rectangle[i].Y) / (rectangle[j].Y - rectangle[i].Y) + rectangle[i].X))
+          inside = !inside;
+      }
+      return inside;
+    }
+  }
+
+  class Line
+  {
+    public int Xmin { get; set; }
+
+    public int Xmax { get; set; }
+
+    public double Ymin { get; set; }
+
+    public double Ymax { get; set; }
+  }
+
+  internal class VertexDouble
+  {
+    public VertexDouble(double x, double y)
+    {
+      X = x;
+      Y = y;
+    }
+
+    public VertexDouble(Vertex vertex) : this(vertex.X, vertex.Y) { }
+
+    //
+    // Summary:
+    //     X coordinate.
+    public double X { get; set; }
+
+    //
+    // Summary:
+    //     Y coordinate.
+    public double Y { get; set; }
+  }
+}
---
+++ GcpVisionLineSegmentation/GcpVisionLineSegmentation/GoogleVisionLineSegmentation.cs
@@ -0,0 +1,172 @@
+﻿using Google.Cloud.Vision.V1;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GcpVisionLineSegmentation
+{
+  public class GoogleVisionLineSegmentation
+  {
+    public static List<string> GetLineSegmentation(IReadOnlyList<EntityAnnotation> data)
+    {
+      var mergedArray = Initialization(data);
+
+      CoordinatesHelper.GetBoundingPolygon(mergedArray);
+      CoordinatesHelper.CombineBoundingPolygon(mergedArray);
+
+      // This does the line segmentation based on the bounding boxes
+      return ConstructLineWithBoundingPolygon(mergedArray);
+    }
+
+    static List<ReceiptEntityAnnotation> Initialization(IReadOnlyList<EntityAnnotation> data)
+    {
+      // Computes the maximum y coordinate from the identified text blob
+      int yMax = data[0].BoundingPoly.Vertices.Max(y => y.Y);
+      CoordinatesHelper.InvertAxis(data, yMax);
+
+      // The first index refers to the auto identified words which belongs to a sings line
+      List<string> lines = data[0].Description.Split('\n').Reverse().ToList();
+
+      // gcp vision full text and remove the zeroth element which gives the total summary of the text
+      // reverse to use lifo, because array.shift() will consume 0(n)
+      var rawText = data.Skip(1).Reverse().ToList();
+
+      return GetMergedLines(lines, rawText);
+    }
+
+    // TODO implement the line ordering for multiple words
+    static List<string> ConstructLineWithBoundingPolygon(List<ReceiptEntityAnnotation> mergedArray)
+    {
+      List<string> finalArray = new List<string>();
+
+      for (int i = 0; i < mergedArray.Count; i++)
+      {
+        if (!mergedArray[i].Matched)
+        {
+          if (mergedArray[i].Match.Count == 0)
+          {
+            finalArray.Add(mergedArray[i].Description);
+          }
+          else
+          {
+            // arrangeWordsInOrder(mergedArray, i);
+            // let index = mergedArray[i]['match'][0]['matchLineNum'];
+            // let secondPart = mergedArray[index].description;
+            // finalArray.push(mergedArray[i].description + ' ' +secondPart);
+            finalArray.Add(ArrangeWordsInOrder(mergedArray, i));
+          }
+        }
+      }
+      return finalArray;
+    }
+
+    static string ArrangeWordsInOrder(List<ReceiptEntityAnnotation> mergedArray, int k)
+    {
+      StringBuilder mergedLine = new StringBuilder();
+      // let wordArray = [];
+      var line = mergedArray[k].Match;
+      // [0]['matchLineNum']
+      for (int i = 0; i < line.Count; i++)
+      {
+        int index = line[i].MatchLineNum;
+        string matchedWordForLine = mergedArray[index].Description;
+
+        int mainX = mergedArray[k].Vertices[0].X;
+        int compareX = mergedArray[index].Vertices[0].X;
+
+        if (compareX > mainX)
+        {
+          mergedLine.Append(mergedArray[k].Description + ' ' + matchedWordForLine);
+        }
+        else
+        {
+          mergedLine.Append(matchedWordForLine + ' ' + mergedArray[k].Description);
+        }
+      }
+      return mergedLine.ToString();
+    }
+
+    static List<ReceiptEntityAnnotation> GetMergedLines(List<string> lines, List<EntityAnnotation> rawText)
+    {
+      List<ReceiptEntityAnnotation> mergedArray = new List<ReceiptEntityAnnotation>();
+      while (lines.Count != 1)
+      {
+        string
+          l = lines.Pop(),
+          l1 = l,
+          data = string.Empty;
+
+        ReceiptEntityAnnotation mergedElement = null;
+
+        while (rawText.Count > 0)
+        {
+          EntityAnnotation wElement = rawText.Pop();
+          string description = wElement.Description;
+
+          int index = l.IndexOf(description);
+          // check if the word is inside
+          l = l.Substring(index + description.Length);
+          if (mergedElement == null)
+          {
+            // set starting coordinates
+            mergedElement = new ReceiptEntityAnnotation(/*wElement.BoundingPoly.Vertices*/)
+            {
+              Description = description,
+              Vertices = wElement.BoundingPoly.Vertices
+            };
+          }
+          if (string.IsNullOrEmpty(l))
+          {
+            // set ending coordinates
+            mergedElement.Description = l1;
+            mergedElement.Vertices[1] = wElement.BoundingPoly.Vertices[1];
+            mergedElement.Vertices[2] = wElement.BoundingPoly.Vertices[2];
+            mergedArray.Add(mergedElement);
+            break;
+          }
+        }
+      }
+      return mergedArray;
+    }
+  }
+
+  class ReceiptEntityAnnotation
+  {
+    public ReceiptEntityAnnotation()
+    {
+      Match = new List<MatchLine>();
+    }
+
+    public string Description { get; set; }
+
+    public IList<Vertex> Vertices { get; set; }
+
+    public VertexDouble[] BigBB { get; set; }
+
+    public List<MatchLine> Match { get; set; }
+
+    public bool Matched { get; set; }
+  }
+
+  class MatchLine
+  {
+    public int MatchCount { get; set; }
+
+    public int MatchLineNum { get; set; }
+  }
+
+  static class ListExtension
+  {
+    public static T Pop<T>(this IList<T> list)
+    {
+      return PopAt(list, list.Count - 1);
+    }
+
+    public static T PopAt<T>(this IList<T> list, int index)
+    {
+      T r = list[index];
+      list.RemoveAt(index);
+      return r;
+    }
+  }
+}
